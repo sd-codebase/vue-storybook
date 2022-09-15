ab84674135193a6e287fbe1d2dd0a8c0c9e5523f
Shrikrishna Dhas <shrikrishna.dhas@simplifai.ai>
2022-09-15 18:20:18 +0530

    create vue storybook

---
+++ src/components/SubText.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        props: ['size', 'variant', 'subtext'],
+    }
+</script>
+
+<template>
+    <div :class="[size, variant]">
+        <span>{{subtext}}</span>
+    </div>
+</template>
